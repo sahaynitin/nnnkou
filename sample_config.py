c36f8c3fb558493957cd67c60eb9f4cea94270b6
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-21 20:29:08 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -36,5 +36,5 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "")
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    MUST_JOIN = os.environ.get("MUST_JOIN", "")
+    # mongodb url
    
