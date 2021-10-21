a41daaa927694962fd9c24fbacadd1d67517d0e1
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-21 20:15:13 +0530

    Update must_join.py

--- plugins/must_join.py
+++ plugins/must_join.py
@@ -1,4 +1,7 @@
-from forcesub import must_join
+from sample_config import MUST_JOIN
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 
 @Client.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
