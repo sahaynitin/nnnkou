8faa812f0384d6dee6fa2299ef3e885458d124e3
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-22 11:25:23 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,7 +12,7 @@ class Config(object):
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Update channel for Force Subscribe
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 2097152000
