d12b266ea1540a427f8651357fd0ea3f418945bf
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-01 14:11:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,5 +22,5 @@ async def start(bot, update):
         text=Translation.START_TEXT.format(update.from_user.mention),
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_to_message_id=update.message_id,
-        reply_markup=Translation.START_BUTTONS
+        reply_to_message_id=update.message_id
+        #reply_markup=Translation.START_BUTTONS#
