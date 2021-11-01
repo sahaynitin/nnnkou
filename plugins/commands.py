8698e48af86996e19387998bd229a0be6c74f59e
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-01 13:51:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -14,3 +14,8 @@ async def start(bot, update):
     forcesub = await ForceSub(bot, update)
     if forcesub == 400:
         return
+await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
