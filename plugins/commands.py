06f5c0d83ea269ddd675c05fa0395a8385f90a7e
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-01 13:46:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -7,6 +7,11 @@ from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, UserNotParticipant
-from plugins.forcesub import ForceSub
+
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
