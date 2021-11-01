eceef4862492ea9db561e220a93a51e80e2e432b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-01 17:36:30 +0530

    Update yt_dlp_echo.py

--- plugins/yt_dlp_echo.py
+++ plugins/yt_dlp_echo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K | X-Noid | @DC4_WARRIOR | Tellybots_4u
-from helper_func.forcesub import ForceSub
+from helper_funcs.forcesub import ForceSub
 # the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
