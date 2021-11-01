696cd9704ec4575414563c02607c41d04589c03c
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-01 17:22:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,17 +10,4 @@ from pyrogram.errors import FloodWait, UserNotParticipant
 from database.adduser import Adduser #from plugins.forcesub import ForceSub
 from plugins.forcesub import ForceSub
 
-@Client.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):
-    # logger.info(update)
-    await AddUser(bot, update)
-    forcesub = await ForceSub(bot, update)
-    if forcesub == 400:
-        return
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(update.from_user.mention),
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-        
+
