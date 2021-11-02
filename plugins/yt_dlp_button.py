d3204f40f03392cda81233170aeeb9a8f2853b6b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-02 07:36:08 +0530

    Update yt_dlp_button.py

--- plugins/yt_dlp_button.py
+++ plugins/yt_dlp_button.py
@@ -163,7 +163,7 @@ async def yt_dlp_call_back(bot, update):
             message_id=update.message.message_id,
             text=error_message
         )
-                return True
+       return False
     if t_response:
         # logger.info(t_response)
         os.remove(save_ytdl_json_path)
