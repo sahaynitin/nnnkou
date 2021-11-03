6f7e1ab67701cbc2753bd28ec4ed3e42e44e5efc
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-03 20:01:04 +0530

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -1,8 +1,8 @@
-from pyrogram import Client
+from pyrogram import Pyrogram
 from database.access import client
 from pyrogram.types import Message
 
 
-async def AddUser(bot: Client, update: Message):
+async def AddUser(bot: Pyrogram, update: Message):
     if not await client.is_user_exist(update.from_user.id):
            await client.add_user(update.from_user.id)
